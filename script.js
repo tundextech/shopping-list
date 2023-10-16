7fd7fec87e6a25ae14d283f774daadb5a0e72038
tundeajayi <t.ajayi@cgiar.org>
2023-10-16 18:15:05 +0100

    Added Clear UI Code

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
-
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 
 function addItem(e){
@@ -18,10 +19,12 @@ const button = createButton('remove-item btn-link text-red');
 li.appendChild(button);
 
 itemList.appendChild(li);
+resetUI();
 itemInput.value = '';
 }
 
 function createButton (classes){
+  
  const button = document.createElement('button');
  button.className = classes;
  const icon = createIcon('fa-solid fa-xmark');
@@ -34,6 +37,42 @@ function createIcon (classes){
     icon.className = classes;
     return icon;
 }
+
+function removeItem(e){
+   if(e.target.parentElement.classList.contains('remove-item')){
+
+    if(confirm('Are you sure')){
+        e.target.parentElement.parentElement.remove();
+
+        resetUI();
+    }
+
+   }
+}
+
+function clearItem(e){
+   // itemList.innerHTML = ''; (The second method below)
+   while(itemList.firstChild){
+    itemList.removeChild(itemList.firstChild);
+   }
+   resetUI();
+}
+
+function resetUI (){
+    const items = itemList.querySelectorAll('li');
+    if(items.length === 0){
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+}
+
 //Eventlisteners
 
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItem);
+
+resetUI();
